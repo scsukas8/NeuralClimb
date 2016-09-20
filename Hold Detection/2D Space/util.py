85c72d1a63681881f1a546da072ebaf165a20b39
scsukas8 <sean.csukas@gmail.com>
2016-09-20 13:42:30 -0400

    Introduce 3D space
    Added 3d model of rock wall

---
+++ Hold Detection/2D Space/util.py
@@ -0,0 +1,14 @@
+import matplotlib as plt
+import cv2
+from PIL import Image
+import Tkinter as tk, Tkconstants, tkFileDialog
+
+
+def openFile():
+	root = tk.Tk()
+	root.withdraw()
+	file_path = tkFileDialog.askopenfilename()
+	return file_path
+
+def showim(title, im):
+	cv2.imshow(title,im )
