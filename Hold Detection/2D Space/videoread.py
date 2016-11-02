e4df57fc8d26ca5bbd495ee587d44cb0828f6440
scsukas8 <sean.csukas@gmail.com>
2016-11-02 12:49:57 -0400

    Revised workaroud using video frames instead of video.

---
+++ Hold Detection/2D Space/videoread.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+from holdDetector import findHolds, findColors, plotColors, buildDetector
+
+import os.path
+import time
+#cap = cv2.VideoCapture(0)
+path = "C:/Users/SeanC/Documents/GitHub/NeuralClimb/Hold Detection/2D Space/StopMoti2001.mpeg"
+
+abspath =  os.path.abspath(path)
+print os.path.exists(abspath)
+print abspath
+cap = cv2.VideoCapture(abspath)
+
+print cap.isOpened()
+
+i = 0
+detector = buildDetector()
+
+while not (cap.isOpened()):
+    i+=1
+
+    file_path = "frames_" + "%04d" % i + ".png"
+    print file_path
+    frame = cv2.imread(file_path,1)
+
+
+
+
+    #Find keypoints
+    keypoints, _ = findHolds(frame,detector)
+
+    colors = findColors(frame,keypoints)
+
+
+    frameWithKeypoints = cv2.drawKeypoints(frame,keypoints,-1,[255,0,0])
+    results = np.concatenate((frame, frameWithKeypoints), axis=0)
+    cv2.imshow('frame',results)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
