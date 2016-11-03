4a70d615f22ff916ee853b8e998226e97c2597cc
scsukas8 <sean.csukas@gmail.com>
2016-11-03 13:23:05 -0400

    documented process

--- Hold Detection/2D Space/WereDoingItLive.py
+++ Hold Detection/2D Space/WereDoingItLive.py
@@ -7,24 +7,28 @@ from holdDetector import findHolds, findColors, plotColors, buildDetector
 import os.path
 import time
 
-
+# Determine output path of video
 i = 0
 directory = "C:/Users/SeanC/Documents/GitHub/NeuralClimb/Hold Detection/2D Space/webcam/"
 abspath = os.path.abspath(directory)
 
+# Increment video number to not overwrite old files.
 while (os.path.exists(abspath)):
     i+=1
     relpath = "Live" + "%04d" % i +".avi"
     path = directory + relpath
     abspath =  os.path.abspath(path)
 
+# Start webcam capture
 cap = cv2.VideoCapture(0)
 # Define the codec and create VideoWriter object
 out = cv2.VideoWriter("webcam\\"+relpath,-1, 15.0, (640*2, 960/2),True)
 detector = buildDetector()
 
-while (True):
 
+while (cap.isOpened()):
+    
+    # Pull in frame from webcam
     retval, frame = cap.read()
     if retval == False:
         break
@@ -32,13 +36,16 @@ while (True):
     #Find keypoints
     keypoints, _ = findHolds(frame,detector)
 
-    #colors = findColors(frame,keypoints)
-
+    #Draw Keypoints onto frame
     frameWithKeypoints = cv2.drawKeypoints(frame,keypoints,-1,[0,0,255])
+
+    #Display frame with keypoint frame side by side.
     results = np.concatenate((frame, frameWithKeypoints), axis=1)
     
+    # Write image to video out
     out.write(results)
 
+    #Display for user.
     cv2.imshow('frame',results)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
