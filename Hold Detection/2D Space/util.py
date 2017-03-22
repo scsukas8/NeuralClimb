78be30d128a67894b2b2a262b262ec640bbed1d8
scsukas8 <sean.csukas@gmail.com>
2017-03-22 16:48:45 -0400

    Improved Util files

--- Hold Detection/2D Space/util.py
+++ Hold Detection/2D Space/util.py
@@ -1,4 +1,4 @@
-#Utility file used for Neural Climb Project
+#Utility file with convenient functions
 
 import matplotlib as plt
 import cv2
@@ -7,11 +7,51 @@ import Tkinter as tk, Tkconstants, tkFileDialog
 
 
 def openFile():
-	root = tk.Tk()
-	root.withdraw()
-	file_path = tkFileDialog.askopenfilename()
-	return file_path
+    root = tk.Tk()
+    root.withdraw()
+    file_path = tkFileDialog.askopenfilename()
+    return file_path
 
-def showim(title, im):
-	cv2.imshow(title,im )
-	cv2.waitKey(0)
+def openImage(file_path = None):
+    # If no file path given, open dialog
+    if file_path is None:
+        file_path = openFile()
+    
+    # If stil no file path chosen, quit
+    if file_path == None:
+        return None
+
+    image = cv2.imread(file_path,1)
+    return image
+
+def grab_from_webcam():
+    # Start webcam capture
+    cap = cv2.VideoCapture(0)
+
+    # Pause for webcam to adjust
+    time.sleep(1)
+
+    # Grabs a frame from the capture
+    retval, frame = cap.read()
+
+    # Release webcam
+    cap.release()
+
+    return (retval, frame)
+
+def grab_from_file(file_path = None):
+    frame = openImage(file_path)
+    retval = not frame is None
+
+    return (retval, frame)
+
+def resize(image):
+    image = cv2.resize(img, (y,x))
+    return image
+
+def showImage(image, title = "Image"):
+    if image is None:
+        "Warning: Image is None"
+        return
+    cv2.imshow(title,image)
+    cv2.waitKey(0)
